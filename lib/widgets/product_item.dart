54749084dbe760a0510d45c749f9d0a376e63c4d
IBAD ASIF <ibadasif@gmail.com>
2021-08-09 23:58:55 +0500

    Snackbar added to product screen for undoing items in cart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -56,6 +56,23 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item to the cart',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                    textColor: Colors.green,
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
